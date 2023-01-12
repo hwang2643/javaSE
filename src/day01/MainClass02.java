dbdf57ba6145b7415ff036eb027b90c205db8d45
ITSC <ITSC@DESKTOP-ONE4R81>
2023-01-12 21:51:30 +0900

    첫 커밋

---
+++ src/day01/MainClass02.java
@@ -0,0 +1,22 @@
+package day01;
+/*
+ * 여러줄 주석 - 컴퓨터가 인식하지 않는 영역
+ * 
+ * 
+ */
+
+// (한줄 주석)자바코드 시작 class 입니다.
+// 자바 코드는 class 예약어 { } 안에 정의
+public class MainClass02 {
+	/**
+	 * JavaDoc 주석
+	 * 메인메소드 - 자바 처음 실행되는 메소드
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Console 출력 명령어
+		System.out.println("안녕하세요");
+		
+	}
+
+}
