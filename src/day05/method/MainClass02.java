303d3557f5d7e1d09d96209a0331f2fa2fbf71be
ITSC <itsc@google.com>
2023-01-26 21:47:43 +0900

    day5

--- src/day05/method/MainClass02.java
+++ src/day05/method/MainClass02.java
@@ -1,9 +1,32 @@
 package day05.method;
 
-/*
- * 
- */
 
 public class MainClass02 {
+	public static String name;  // 전역변수 name 선언
+	
+	public static void main(String[] args) {
+		
+		setName("잠만보");   // set메소드를 통해 전역변수에 값 대입
+		String name = getName();   //get메소드를 통해 전역변수 값 불러오기
+		System.out.println(name);
+		
+		int sum = sum(1,2,3);
+		System.out.println("합계 : " + sum);
+	}
+	
+	// 인자 x, 리턴 o
+	public static String getName() {
+//		name = "피카츄";
+		return name;
+	}
+	
+	public static void setName(String name) {
+		MainClass02.name = name;
+	}
+	
+	// 인자 o, 리턴 o
+	public static int sum(int a, int b, int c) {
+		return a + b + c;
+	}
 
 }
